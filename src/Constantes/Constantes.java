043f9fdff94b9ea7e5b0669185803811888c7590
jhonnypadilla <96629248+jhonnypadilla@users.noreply.github.com>
2023-10-30 14:22:05 -0500

    avance configuracion protocolos

---
+++ src/Constantes/Constantes.java
@@ -0,0 +1,21 @@
+package Constantes;
+
+import java.util.ArrayList;
+
+public class Constantes {
+
+    public static final String GILBARCO = "GILBARCO";
+    public static final String WAYNE = "WAYNE";
+    public static final String BLUE_SKY = "BLUE SKY";
+    
+    public static final String RESPUESTA_BLUE_SKY = "foreal";
+
+    public static ArrayList<String> obtenerProtocolos() {
+        ArrayList<String> protocolos = new ArrayList();
+        protocolos.add(GILBARCO);
+        protocolos.add(WAYNE);
+        protocolos.add(BLUE_SKY);
+        return protocolos;
+    }
+
+}
